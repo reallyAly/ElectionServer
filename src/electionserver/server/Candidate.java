9cd3f14e372ebc11a2f31e06ff37991f46546b60
Alysson Victor <alysson@pop-os.localdomain>
2022-05-28 20:46:45 -0300

    Add candidate class

---
+++ src/electionserver/server/Candidate.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package electionserver.server;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author alysson
+ */
+public class Candidate implements Serializable {
+    
+    private int id;
+    
+    private String name;
+
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public Candidate() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+   
+}
