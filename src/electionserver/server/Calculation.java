ed866611b4b70d8352ddfe33f1602fe943016c51
Alysson Victor <alysson@pop-os.localdomain>
2022-05-29 00:32:08 -0300

    Calculation Thread and Calculation View

---
+++ src/electionserver/server/Calculation.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package electionserver.server;
+
+import electionserver.server.model.Candidate;
+import electionserver.server.model.Urn;
+import electionserver.server.model.Votes;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author alysson
+ */
+public class Calculation extends Thread{
+    
+    private JTable candidatesTable;
+    
+    private ArrayList<Candidate> candidates;
+    
+    private ArrayList<Urn> urns;
+    
+    public Calculation(JTable candidatesTable, ArrayList<Urn> urns, ArrayList<Candidate> candidates) {
+        this.candidatesTable = candidatesTable;
+        this.candidates = candidates;
+        this.urns = urns;
+    }
+    
+    @Override
+    public void run() {
+        
+        while(true) {
+            
+            try {
+                
+               for(Candidate candidate: this.candidates) {
+                   for(Urn urn: this.urns){
+                       for(Votes votes: urn.getVotes()) {
+                           for(Candidate urnCandidate: votes.getCandidates()) {
+                               if(candidate.getId() == urnCandidate.getId()) {
+                                  candidate.setVotes(candidate.getVotes() + urnCandidate.getVotes());
+                               }
+                           }
+                       }
+                   }
+               }
+               
+               
+               for(int i = 0; i < this.candidates.size(); i++) {
+                   this.candidatesTable.setValueAt(this.candidates.get(i).getName(), i, 0);
+                   this.candidatesTable.setValueAt(this.candidates.get(i).getVotes(), i, 1);
+               }
+
+               sleep(5000);
+
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Calculation.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+    }
+    
+}
