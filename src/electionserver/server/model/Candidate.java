a77b4202c01ac133232635d3a73340f57d752550
Alysson Victor <alysson@pop-os.localdomain>
2022-05-28 23:02:17 -0300

    Add model package and move classes

--- src/electionserver/server/model/Candidate.java
+++ src/electionserver/server/model/Candidate.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package electionserver.server;
+package electionserver.server.model;
 
 import java.io.Serializable;
 
--- src/electionserver/server/model/Votes.java
+++ src/electionserver/server/model/Votes.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package electionserver.server;
+package electionserver.server.model;
 
+import electionserver.server.model.Candidate;
 import java.io.Serializable;
 import java.util.ArrayList;
 
